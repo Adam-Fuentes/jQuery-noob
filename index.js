6feddec598fed59e4e67faeab21c6f43c894e54c
Adam Fuentes <66355715+Adam-Fuentes@users.noreply.github.com>
2020-10-28 09:43:52 +0100

    add some jQuerys

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-$("h1").css("color", "red");
+
+//$("h1").css("color", "grey"); //seleciona TODOS los h1 y les cambia el color
+
+//console.log($("h1").css("color")); //SI PONES SOLO UNA PROPIEDAD CON EL LOG TE DA LA PROPIEDAD
+
+/*
+$("h1").addClass("big-title");
+
+$("h1").hasClass("big-title");
+
+$("button").text("no clickes");
+*/
+/*
+
+al clicar en cualquier botón cambia el color a rojo del h1
+
+$("button").click(function() {
+    $("h1").css("color", "Red");
+});
+
+
+*/
